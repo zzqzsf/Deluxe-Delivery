34cbde525bb1e4f995bdbd04308ec1113dacc41a
zzqzsf <1014221651@qq.com>
2019-09-07 17:02:55 +0800

    first commit

---
+++ src/main/java/org/lanqiao/deluxedelivery/DeluxeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.lanqiao.deluxedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeluxeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeluxeDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lanqiao/deluxedelivery/DeluxeDeliveryApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lanqiao.deluxedelivery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeluxeDeliveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
