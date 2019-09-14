e2b6ed08a1e813219e96da3c13bb466e00bb9834
bianxingli <1119268834@qq.com>
2019-09-14 21:18:52 +0800

    改git

--- src/main/java/org/lanqiao/service/ShopService.java
+++ src/main/java/org/lanqiao/service/ShopService.java
@@ -1,4 +1,11 @@
 package org.lanqiao.service;
 
+import org.lanqiao.entity.Shop;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface ShopService {
+    public List<Shop> selectShopInfo(int shopId);
 }
--- src/main/java/org/lanqiao/service/ShopServiceImpl.java
+++ src/main/java/org/lanqiao/service/ShopServiceImpl.java
@@ -1,4 +1,17 @@
 package org.lanqiao.service;
 
-public class ShopServiceImpl {
+import org.lanqiao.entity.Shop;
+import org.lanqiao.mapper.ShopMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.xml.ws.Service;
+import java.util.List;
+
+public class ShopServiceImpl implements ShopService {
+    @Autowired
+    ShopMapper shopMapper;
+    @Override
+    public List<Shop> selectShopInfo(int shopId) {
+        return null;
+    }
 }
