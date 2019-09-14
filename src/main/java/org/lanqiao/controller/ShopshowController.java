9e711ce76de4c51c3e60ca1af6c7f93894d7f9e8
chenwe12 <619092800@qq.com>
2019-09-14 20:38:36 +0800

    9.14陈文修改pom及其他3

--- src/main/java/org/lanqiao/controller/ShopshowController.java
+++ src/main/java/org/lanqiao/controller/ShopshowController.java
@@ -3,7 +3,7 @@ package org.lanqiao.controller;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.lanqiao.entity.Shop;
-import org.lanqiao.service.ShopshowService;
+import org.lanqiao.service.ShopShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +14,11 @@ import java.util.List;
 @RestController
 public class ShopshowController {
 @Autowired
-ShopshowService shopshowService;
+ShopShowService shopShowService;
 
     @RequestMapping("/getgoods")
     List<Shop> GetAllShops() {
-        return shopshowService.GetAllShops();
+        return shopShowService.GetAllShops();
     }
 
 
@@ -27,7 +27,7 @@ ShopshowService shopshowService;
 
         PageHelper.startPage(pageNum, 3);
 
-        List<Shop> shareList = shopshowService.GetAllShops();
+        List<Shop> shareList = shopShowService.GetAllShops();
 
         PageInfo<Shop> pageInfo = new PageInfo<>(shareList);
 
