9a027e71441318f8f58d06d0e2f3dc78775b7f7e
chenwe12 <619092800@qq.com>
2019-09-14 22:09:14 +0800

    chenwen

--- src/main/java/org/lanqiao/service/ShopShowServiceImpl.java
+++ src/main/java/org/lanqiao/service/ShopShowServiceImpl.java
@@ -3,24 +3,15 @@ package org.lanqiao.service;
 import org.lanqiao.entity.Shop;
 import org.lanqiao.mapper.ShopMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-<<<<<<< HEAD
-public class ShopshowServiceImpl implements ShopshowService {
-=======
-public class ShopShowServiceImpl implements ShopShowService {
->>>>>>> parent of db70341... 改
+public class ShopShowServiceImpl  implements ShopShowService{
     @Autowired
     ShopMapper shopMapper;
     @Override
     public List<Shop> GetAllShops() {
         return shopMapper.GetAllShops();
-<<<<<<< HEAD
-//  hhggf
-=======
->>>>>>> parent of db70341... 改
+
     }
 }
