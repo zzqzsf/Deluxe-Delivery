0731a12667fb7deaf954e1ca9c917b443f2b454b
daiyuqing <935107920@qq.com>
2019-09-14 15:43:59 +0800

    登录注册找回密码三个功能完成

--- src/main/java/org/lanqiao/DeluxeDeliveryApplication.java
+++ src/main/java/org/lanqiao/DeluxeDeliveryApplication.java
@@ -1,11 +1,12 @@
 package org.lanqiao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.lanqiao.mapper")
 public class DeluxeDeliveryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeluxeDeliveryApplication.class, args);
 	}
---
+++ src/main/java/org/lanqiao/service/CustomService.java
@@ -0,0 +1,13 @@
+package org.lanqiao.service;
+
+import org.lanqiao.entity.Custom;
+import org.springframework.stereotype.Service;
+
+
+public interface CustomService {
+    int checkTel(String tel);
+    int insertCustom(Custom custom);
+    int insertCustom1(String cusTel);
+    int checkCustom(Custom custom);
+    int updatePass(String cusTel,String cusPwd);
+}
