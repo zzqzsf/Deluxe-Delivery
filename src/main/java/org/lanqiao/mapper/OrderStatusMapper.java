aa12700525a64264be01de66fb07ffad0169e54e
bianxingli <1119268834@qq.com>
2019-09-15 17:46:13 +0800

    OrderStatus

---
+++ src/main/java/org/lanqiao/mapper/OrderStatusMapper.java
@@ -0,0 +1,9 @@
+package org.lanqiao.mapper;
+
+import org.lanqiao.entity.OrderStatus;
+
+public interface OrderStatusMapper {
+    int insert(OrderStatus record);
+
+    int insertSelective(OrderStatus record);
+}
